238667b5a2e019aff2fc4cb482eea4ffbc85b5c0
AsafSivan1 <asafisivan@gmail.com>
2020-06-04 21:58:23 +0300

    Add files via upload

---
+++ minecraft/code.py
@@ -0,0 +1,150 @@
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+from tqdm.auto import tqdm
+
+
+
+global texture
+block = 'dirt'
+texture = 'dirt'
+app = Ursina()
+
+
+
+player = FirstPersonController()
+
+
+
+
+#window settings
+window.fullscreen = True
+window.fps_counter.enabled = False
+window.exit_button.visible = False
+window.title = 'Minecraft-Asaf'
+
+
+
+
+
+
+
+
+
+#block placer/breaker
+class Block(Button):
+    met = ['wood','stone']
+    def __init__(self, position=(0,0,0),texture='stone'):
+        super().__init__(
+            parent = scene,
+            position = position,
+            model = 'cube',
+            origin_y = .01,
+            texture =texture,
+            color = color.color(0, 0, .9),
+            highlight_color = color.lime,
+        )
+
+
+    def input(self, key):
+        global texture
+        if str(key) == '1':
+            texture = 'dirt'
+        if str(key) == '2':
+            texture = 'wood'
+        if str(key) == '3':
+            texture = 'stone'
+        if str(key) == '4':
+            texture = 'stone_bricks'
+        if str(key) == '5':
+            texture = 'oak_log'
+        if str(key) == '6':
+            texture = 'wool'
+        if str(key) == '7':
+            texture = 'obsidian'
+        if str(key) == '8':
+            texture = 'granite'
+        if str(key) == '9':
+            texture = 'bedrock'
+
+
+        if self.hovered:
+            if key == 'right mouse down':
+                try:
+                    block = Block(position=self.position + mouse.normal,texture=texture)
+                except NameError:
+                    texture = 'dirt'
+                    block = Block(position=self.position + mouse.normal, texture=texture)
+
+            if key == 'left mouse down':
+                destroy(self)
+
+#Inventory
+class Inventory(Entity):
+    def __init__(self):
+        super().__init__(
+            parent = camera.ui,
+            model = 'quad',
+            scale = (.9, .1),
+            origin = (-.5, .5),
+            position = (-0.45,-0.4),
+            texture = 'widgets',
+            texture_scale = (9,1),
+            color = color.dark_gray
+            )
+
+
+
+
+
+class Items(Entity):
+    def __init__(self,texture,position):
+        super().__init__(
+            parent=camera.ui,
+            model='quad',
+            scale=(.1, .1),
+            origin=(-.5, .5),
+            position=(-position, -0.4),
+            texture=texture,
+            texture_scale=(1, 1),
+            color= color.white
+
+
+        )
+
+
+blocks_List = ['dirt','wood','stone','stone_bricks','oak_log','wool','obsidian','granite','bedrock']
+startPos = 0.45
+for i in range(9):
+    items = Items(blocks_List[i],startPos)
+    startPos -= 0.1
+
+
+inventory = Inventory()
+inventory.texture
+
+
+#movind up/down
+def update():
+    player.y += held_keys['space'] * time.dt * 10
+    player.y -= held_keys['shift'] * time.dt * 10
+
+
+
+
+#making platform
+#chaning range to bigger x size platform
+for z in tqdm(range(15)):
+    #chaning range to bigger x size platform
+    for x in range(15):
+        i = 0
+        block = Block(position=(x, 0, z), texture='bedrock')
+        # chaning range to bigger y size platform
+        for k in range(4):
+            i += 1
+            block = Block(position=(x, i, z), texture='dirt')
+
+
+
+
+
+app.run()
